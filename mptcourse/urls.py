d8aac11ca9a4d530ea1b38095d0ff11b3b54ad7c
Anton <safronov_anton@icloud.com>
2026-02-18 15:37:53 +0300

    ФИКС БИБЛИОТЕК

--- mptcourse/urls.py
+++ mptcourse/urls.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
+#from drf_yasg.views import get_schema_view
+#from drf_yasg import openapi
 from rest_framework import permissions
 from main import views
 from main.metrics_view import metrics_export
